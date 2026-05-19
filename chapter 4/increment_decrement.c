4bf7228c6edb383395c91dbbea963e684b912a9f
vasava aaditya <vasavaaadi561@gmail.com>
2026-05-20 00:49:48 +0530

    .

--- chapter 4/increment_decrement.c
+++ chapter 4/increment_decrement.c
@@ -7,7 +7,10 @@ int main(){
     i = i + 5; // 10
     printf("The value of i is %d\n",i);
 
-    i++; // 11
-    printf("The value of i is %d\n",i);
+    // i++; // 11
+    printf("The value of i is %d\n",++i);
     return 0;
 }
+
+// i++ prints first and then increments (post increment oprators)
+// ++1 increments first and then prints i (post increment oprators)
