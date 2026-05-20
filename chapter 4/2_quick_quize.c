7344e8e190d4c5f0198f2db16897e4041d2bc084
vasava aaditya <vasavaaadi561@gmail.com>
2026-05-20 13:36:48 +0530

    .

---
+++ chapter 4/2_quick_quize.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int  n, i = 1;
+    printf("Enter the value: ");
+    scanf("%d",&n);
+    do
+    {
+      printf("%d\n",i)  ;
+      i++;
+    } while (i<=n);
+    
+    return 0;
+}
